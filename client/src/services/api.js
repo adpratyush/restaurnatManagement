f5ce554c6546e7f9891d5d8725f77115dbb955f9
no1heroat <no1heroat@gmail.com>
2026-08-21 14:12:53 +0545

    Update API configuration for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "hhttps://restaurnat-management-an6e.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
